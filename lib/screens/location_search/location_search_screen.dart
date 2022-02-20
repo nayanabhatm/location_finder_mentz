2085b91130f4b6ec12c8c53e370054901303e389
nayana <nayana.bhat.m@gmail.com>
2022-02-20 19:22:57 +0100

    removed bkg and udpated readme

--- lib/screens/location_search/location_search_screen.dart
+++ lib/screens/location_search/location_search_screen.dart
@@ -24,114 +24,104 @@ class _LocationsSearchScreenState extends State<LocationsSearchScreen> {
     ThemeData themeData = Theme.of(context);
     return SafeArea(
       child: Scaffold(
-        body: DecoratedBox(
-          decoration: BoxDecoration(
-            image: const DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(Constants.bkgImage),
-              opacity: 0.8,
-            ),
-            color: themeData.primaryColorLight,
-          ),
-          child: Column(
-            children: [
-              LocationsFinderBanner(
-                title: Constants.stopFinderTitle,
-                childWidget: Padding(
-                  padding: const EdgeInsets.all(Styles.padding20),
-                  child: TextFormField(
-                    controller: _textEditingController,
-                    autofocus: false,
-                    autocorrect: false,
-                    style: themeData.textTheme.headline5,
-                    cursorColor: themeData.primaryColorLight,
-                    autovalidateMode: AutovalidateMode.disabled,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        FontAwesomeIcons.search,
-                        color: themeData.primaryColorLight.withOpacity(0.5),
-                      ),
-                      hintText: Constants.hintText,
-                      hintStyle: themeData.textTheme.bodyText1?.copyWith(
-                        color: themeData.primaryColorLight,
-                      ),
-                      suffixIcon: IconButton(
-                        icon: const FaIcon(FontAwesomeIcons.times),
-                        color: const Color(Styles.white),
-                        onPressed: () {
-                          _textEditingController.clear();
-                          BlocProvider.of<LocationsCubit>(context)
-                              .resetLocations();
-                        },
-                      ),
-                      border: _getBorder(context),
-                      enabledBorder: _getBorder(context),
-                      focusedBorder: _getBorder(context),
-                      focusColor: themeData.primaryColorLight,
+        body: Column(
+          children: [
+            LocationsFinderBanner(
+              title: Constants.stopFinderTitle,
+              childWidget: Padding(
+                padding: const EdgeInsets.all(Styles.padding20),
+                child: TextFormField(
+                  controller: _textEditingController,
+                  autofocus: false,
+                  autocorrect: false,
+                  style: themeData.textTheme.headline5,
+                  cursorColor: themeData.primaryColorLight,
+                  autovalidateMode: AutovalidateMode.disabled,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.search,
+                      color: themeData.primaryColorLight.withOpacity(0.5),
                     ),
-                    onChanged: (String newLocation) async {
-                      if (newLocation.length > 2) {
+                    hintText: Constants.hintText,
+                    hintStyle: themeData.textTheme.bodyText1?.copyWith(
+                      color: themeData.primaryColorLight,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: const FaIcon(FontAwesomeIcons.times),
+                      color: const Color(Styles.white),
+                      onPressed: () {
+                        _textEditingController.clear();
                         BlocProvider.of<LocationsCubit>(context)
-                            .getLocations(newLocation);
-                      }
-                    },
+                            .resetLocations();
+                      },
+                    ),
+                    border: _getBorder(context),
+                    enabledBorder: _getBorder(context),
+                    focusedBorder: _getBorder(context),
+                    focusColor: themeData.primaryColorLight,
                   ),
+                  onChanged: (String newLocation) async {
+                    if (newLocation.length > 2) {
+                      BlocProvider.of<LocationsCubit>(context)
+                          .getLocations(newLocation);
+                    }
+                  },
                 ),
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(Styles.padding10),
-                  child: BlocConsumer<LocationsCubit, LocationsState>(
-                    listener: (BuildContext context, LocationsState state) {
-                      if (state is LocationsNetworkError) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            duration: const Duration(seconds: 4),
-                            content: Text(
-                              state.message,
-                              style: themeData.textTheme.headline5,
-                            ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(Styles.padding10),
+                child: BlocConsumer<LocationsCubit, LocationsState>(
+                  listener: (BuildContext context, LocationsState state) {
+                    if (state is LocationsNetworkError) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          duration: const Duration(seconds: 4),
+                          content: Text(
+                            state.message,
+                            style: themeData.textTheme.headline5,
                           ),
-                        );
-                      }
-                    },
-                    builder: (BuildContext context, LocationsState state) {
-                      if (state is LocationsError) {
-                        return FailureWidget(message: state.message);
-                      } else if (state is LocationsSuccess) {
-                        return ListView.builder(
-                          itemCount: state.locations.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return LocationTile(
-                              location: state.locations[index],
-                            );
-                          },
-                        );
-                      } else if (state is LocationsLoading) {
-                        return const LoadingIndicator();
-                      } else if (state is LocationsEmpty) {
-                        return Card(
-                          elevation: 6,
-                          child: Center(
-                            child: Text(
-                              Constants.emptyLocations,
-                              textAlign: TextAlign.center,
-                              style: themeData.textTheme.headline6,
-                            ),
-                          ),
-                        );
-                      }
-                      return Text(
-                        Constants.defaultText,
-                        style: themeData.textTheme.headline6,
-                        textAlign: TextAlign.center,
+                        ),
                       );
-                    },
-                  ),
+                    }
+                  },
+                  builder: (BuildContext context, LocationsState state) {
+                    if (state is LocationsError) {
+                      return FailureWidget(message: state.message);
+                    } else if (state is LocationsSuccess) {
+                      return ListView.builder(
+                        itemCount: state.locations.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return LocationTile(
+                            location: state.locations[index],
+                          );
+                        },
+                      );
+                    } else if (state is LocationsLoading) {
+                      return const LoadingIndicator();
+                    } else if (state is LocationsEmpty) {
+                      return Card(
+                        elevation: 6,
+                        child: Center(
+                          child: Text(
+                            Constants.emptyLocations,
+                            textAlign: TextAlign.center,
+                            style: themeData.textTheme.headline6,
+                          ),
+                        ),
+                      );
+                    }
+                    return Text(
+                      Constants.defaultText,
+                      style: themeData.textTheme.headline6,
+                      textAlign: TextAlign.center,
+                    );
+                  },
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
